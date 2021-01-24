08b5350ec104bce5b447dad3e61d302650d30e07
danplatypus <daniel.gohch@hotmail.com>
2021-01-24 20:06:18 +0800

    added test.js, can call ajax api and communicate with azure server successfully

--- test.js
+++ test.js
@@ -1,34 +1,41 @@
 //import axios from "node_modules/axios";
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    alert("hey you clicked the cute ghost!");
-});
-
 //https://teamrev3lations.azurewebsites.net/api/python_azure?code=
 var masterkey = "qX3ocFWaQ0d3xgFfgu84cgKb6PiJvFMoKVmeSfQGNOyKFWab1nT/oA==";
 var httpReqLink = "https://teamrev3lations.azurewebsites.net/api/python_azure?code=" + masterkey;
 
-//call api
-try{
-    // var response = axios.get(`${httpReqLink}`);
-    // alert("yay it worked! " + response.ToString());
-    $.ajax({
-        url:  httpReqLink,
-        type: "GET",
-        username: user,
-        password: pass,
-        contentType: "application/json",
-        timeout: 5000,
-        accepts:"application/json",
-        dataType:"json",
-        crossDomain : true, //mandatory
-        success: function (data, status, jqXHR) {
-        },
-        error: function (jqXHR, status) {
-            // error handler
-            alert("Error: "+status);
-            }
-        });
-} catch (e){
-    alert("cant call api " + e)
-}
+chrome.browserAction.onClicked.addListener(function(tab) {
+    //call api
+    try{
+        // var response = axios.get(`${httpReqLink}`);
+        // alert("yay it worked! " + response.ToString());
+        $.ajax({
+            url:  httpReqLink,
+            type: "GET",
+            data: {
+                name: "daniel",
+                bodyText: "This is legit"
+            },
+            contentType: "application/json",
+            timeout: 5000,
+            crossDomain : true, //mandatory
+            success: function (data, status, jqXHR) {
+                // data
+                // "HTTP Request triggered correctly, try to add a name parameter in your request
+                //  for a personalised response."
+                // status "success"
+                // jqXHR
+                //  {"readyState":4,"responseText":"HTTP Request triggered correctly, 
+                //  try to add a name parameter in your request for a personalised response.",
+                //  "status":200,"statusText":"OK"}
+                alert("done" + JSON.stringify(data));
+            },
+            error: function (jqXHR, status) {
+                // error handler
+                alert("Error: " + JSON.stringify(jqXHR) + JSON.stringify);
+                }
+            });
+    } catch (e){
+        alert("cant call api " + e)
+    }
+});
