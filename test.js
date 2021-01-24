ffb57ed51f641e4df00ed2ea4d2529c25f590832
danplatypus <daniel.gohch@hotmail.com>
2021-01-24 21:07:04 +0800

    now test.js can get url of current page as well

--- test.js
+++ test.js
@@ -5,37 +5,45 @@ var masterkey = "qX3ocFWaQ0d3xgFfgu84cgKb6PiJvFMoKVmeSfQGNOyKFWab1nT/oA==";
 var httpReqLink = "https://teamrev3lations.azurewebsites.net/api/python_azure?code=" + masterkey;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    //call api
-    try{
-        // var response = axios.get(`${httpReqLink}`);
-        // alert("yay it worked! " + response.ToString());
-        $.ajax({
-            url:  httpReqLink,
-            type: "GET",
-            data: {
-                name: "daniel",
-                bodyText: "This is legit"
-            },
-            contentType: "application/json",
-            timeout: 5000,
-            crossDomain : true, //mandatory
-            success: function (data, status, jqXHR) {
-                // data
-                // "HTTP Request triggered correctly, try to add a name parameter in your request
-                //  for a personalised response."
-                // status "success"
-                // jqXHR
-                //  {"readyState":4,"responseText":"HTTP Request triggered correctly, 
-                //  try to add a name parameter in your request for a personalised response.",
-                //  "status":200,"statusText":"OK"}
-                alert("done" + JSON.stringify(data));
-            },
-            error: function (jqXHR, status) {
-                // error handler
-                alert("Error: " + JSON.stringify(jqXHR) + JSON.stringify);
-                }
-            });
-    } catch (e){
-        alert("cant call api " + e)
-    }
+    //get the website url
+    var url;
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+        function(tabs){
+            //this is async so url can only be used inside
+            url = tabs[0].url;
+            //call api
+            try{
+                var response = axios.get(`${httpReqLink}`);
+                alert("yay it worked! " + response.ToString());
+                $.ajax({
+                    url:  httpReqLink,
+                    type: "GET",
+                    data: {
+                        name: "daniel",
+                        bodyText: "This is legit"
+                    },
+                    contentType: "application/json",
+                    timeout: 5000,
+                    crossDomain : true, //mandatory
+                    success: function (data, status, jqXHR) {
+                        // data
+                        // "HTTP Request triggered correctly, try to add a name parameter in your request
+                        //  for a personalised response."
+                        // status "success"
+                        // jqXHR
+                        //  {"readyState":4,"responseText":"HTTP Request triggered correctly, 
+                        //  try to add a name parameter in your request for a personalised response.",
+                        //  "status":200,"statusText":"OK"}
+                        alert("done" + JSON.stringify(data));
+                    },
+                    error: function (jqXHR, status) {
+                        // error handler
+                        alert("Error: " + JSON.stringify(jqXHR) + JSON.stringify);
+                        }
+                    });
+            } catch (e){
+                alert("cant call api " + e)
+            }
+        }
+    );
 });
