de73f07529acdc1c4e3b5805f67d78cfbeae5fbe
danplatypus30 <51561274+danplatypus30@users.noreply.github.com>
2021-01-27 00:58:06 +0800

    added changes, have a popup html, can call api, and display results to frontend

--- popup.js
+++ popup.js
@@ -1,28 +1,20 @@
-//import axios from "node_modules/axios";
 
-//https://teamrev3lations.azurewebsites.net/api/python_azure?code=
-var masterkey = "qX3ocFWaQ0d3xgFfgu84cgKb6PiJvFMoKVmeSfQGNOyKFWab1nT/oA==";
-var httpReqLink = "https://teamrev3lations.azurewebsites.net/api/python_azure?code=" + masterkey;
+function addResults(data){
+    document.getElementById("malware_results").innerHTML = "no malicious links";
+    document.getElementById("legitimacy_results").innerHTML = data;
+}
 
-
-//this doesnt work anymore when html is there
-chrome.browserAction.onClicked.addListener(function(tab) {
-    //get the website url
-    var url;
-    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-        function(tabs){
-            //get the text in the website
-            //call api to get the results
-            //callapi(tabs);
-            //open popup html showing results
-            alert("still calls here");
-        }
-    );
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+    addResults("fake 25%"); //temporary put in because idw to keep calling the api
+    //callapi();
 });
 
-function callapi(tabs){
-    //this is async so url can only be used inside
-    url = tabs[0].url;
+function callapi(){
+    var masterkey = "qX3ocFWaQ0d3xgFfgu84cgKb6PiJvFMoKVmeSfQGNOyKFWab1nT/oA==";
+    var httpReqLink = "https://teamrev3lations.azurewebsites.net/api/python_azure?code=" + masterkey;
     //call api
     try{
         $.ajax({
@@ -44,7 +36,8 @@ function callapi(tabs){
                 //  {"readyState":4,"responseText":"HTTP Request triggered correctly, 
                 //  try to add a name parameter in your request for a personalised response.",
                 //  "status":200,"statusText":"OK"}
-                alert("done" + JSON.stringify(data));
+                //alert("done" + JSON.stringify(data));
+                addResults(data);
             },
             error: function (jqXHR, status) {
                 // error handler
