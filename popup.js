b69e01b8083244d5967c1d84e788ef2f62219fba
danplatypus <daniel.gohch@hotmail.com>
2021-01-31 18:39:22 +0800

    added ammendments to the UI

--- popup.js
+++ popup.js
@@ -1,33 +1,34 @@
 
 function addResults(data){
-    //filter by those between <p> tags
-    const matches = data.match(/<p>(.*?)<\/p>/g); //array of strings between ptags
-    var matchesFormatted = [];
-    for(var a = 0; a < matches.length; a++){
-        //take out the <p> tags
-        var noPtag = matches[a].toString().substr(3, matches[a].length - 7);
-        // //need to take out any other html tags here, including text in between
-        // const htmlStrings = noPtag.match(/<\s*[a-z]*[^>]*>(.*?)<\s*\/\s*[a-z]*>/g); //https://www.regextester.com/27540
-        // if(htmlStrings != null){
-        //     for(var b = 0; b < htmlStrings.length; b++){
-        //         noPtag = noPtag.replace(htmlStrings[b],"");
-        //     }
-        // }
-        //take out single tag html, like <br> <hr>
-        const singlehtmltagstrings = noPtag.match(/<\s*[\/]?\s*[a-z]*[^>]*>/g);
-        if(singlehtmltagstrings != null){
-            for(var c = 0; c < singlehtmltagstrings.length; c++){
-                noPtag = noPtag.replace(singlehtmltagstrings[c],"");
-            }
-        }
-        noPtag = noPtag.replace(/&nbsp;/g, " ");
-        matchesFormatted.push(noPtag);
+    var ifFake = "The website contains fake news";
+    var ifFakeExp = "Our machine learning algorithm has detected fake news on this website";
+    var ifReal = "This website is safe";
+    var ifRealExp = "Nothing wrong has been detected by our algorithms";
+    var legitimacy = false;
+    legitimacy = true;
+    if(legitimacy){
+        document.getElementById("legitimacy_results").innerText = ifReal;
+        document.getElementById("legitimacy_results_exp").innerText = ifRealExp;
+    } else {
+        document.getElementById("legitimacy_results").innerText = ifFake;
+        document.getElementById("legitimacy_results_exp").innerText = ifFakeExp;
     }
-    document.getElementById("legitimacy_results").innerText = matchesFormatted.join(" ");
 }
 
 function addVTResults(data){
-    document.getElementById("malware_results").innerHTML = data;
+    var ifFake = "The website is associated with malicious websites";
+    var ifFakeExp = "VirusTotal has detected malicious presence on this website";
+    var ifReal = "This website is safe";
+    var ifRealExp = "Nothing wrong has been detected by VirusTotal"
+    var malicious = false;
+    malicious = true;
+    if(malicious){
+        document.getElementById("malware_results").innerText = ifReal;
+        document.getElementById("malware_results_exp").innerText = ifRealExp;
+    } else {
+        document.getElementById("malware_results").innerText = ifFake;
+        document.getElementById("malware_results_exp").innerText = ifFakeExp;
+    }
 }
 
 // Add event listeners once the DOM has fully loaded by listening for the
@@ -35,7 +36,7 @@ function addVTResults(data){
 // specific elements when it triggers.
 document.addEventListener('DOMContentLoaded', function () {
     //addResults("fake 25%"); //temporary put in because idw to keep calling the api
-    //callVirusTotalAPI();
+    callVirusTotalAPI();
     //callFakeNewsCheckerAPI();
     //callAzureAPI();
     getTextFromHtml();
@@ -52,9 +53,37 @@ function getTextFromHtml(){
         }, function(results) {
             // results.length must be 1
             var result = results[0];
-            addResults(result);
+            callFakeNewsCheckerAPI(filterOutWordsFromHTML(result));
         });
     });
+}
+
+//returns formatted data
+function filterOutWordsFromHTML(data){
+    //only works if news page uses p tags
+    //filter by those between <p> tags
+    const matches = data.match(/<p(\s[^>]*)?>(.*?)<\s*\/\s*p>/g); //array of strings between <p> or <p class="dfsfd"> tags
+    var matchesFormatted = [];
+    for(var a = 0; a < matches.length; a++){
+        var noPtag = matches[a].toString();
+        //take out all the html tags
+        noPtag = noPtag.replace(/<\s*[\/]?\s*[a-z]*[^>]*>/g, "");
+        // //need to take out any other html tags here, including text in between
+        // const htmlStrings = noPtag.match(/<\s*[a-z]*[^>]*>(.*?)<\s*\/\s*[a-z]*>/g); //https://www.regextester.com/27540
+        // if(htmlStrings != null){
+        //     for(var b = 0; b < htmlStrings.length; b++){
+        //         noPtag = noPtag.replace(htmlStrings[b],"");
+        //     }
+        // }
+        //change tabs to space
+        noPtag = noPtag.replace(/&nbsp;/g, " ");
+        //remove if only consists of one word
+        //if the string consists of 4 or more whitespaces
+        if(noPtag.split(/\s/g).length >= 4){
+            matchesFormatted.push(noPtag);
+        } 
+    }
+    return matchesFormatted.join(" ");
 }
 
 function callVirusTotalAPI(){
@@ -132,10 +161,10 @@ function callVirusTotalAPI(){
 
 // call andre's backend AI fake news checker
 //{"readyState":4,"responseText":"Real","status":200,"statusText":"success"}
-function callFakeNewsCheckerAPI(){
+function callFakeNewsCheckerAPI(textToCheck){
     var fakenewsurl = "https://apollorevelation.azurewebsites.net/api";
     var dataString = {
-        "data":"The dog had a lot of work to do. He was co-starring in a political ad that had to showcase the candidate’s good-natured warmth. But the ad also needed to deflect an onslaught of racialized attacks without engaging them directly, and to convey to white voters in Georgia that the Black pastor who led Ebenezer Baptist Church could represent them, too. Of course, Alvin the beagle couldn’t have known any of that when he went for a walk with the Rev. Raphael Warnock last fall as a film crew captured their time together in a neighborhood outside Atlanta. Tugging a puffer-vest-clad Mr. Warnock for an idealized suburban stroll — bright sunshine, picket fencing, an American flag — Alvin would appear in several of Mr. Warnock’s commercials pushing back against his Republican opponent in the recent Georgia Senate runoffs. In perhaps the best known spot, Mr. Warnock, a Democrat, deposits a plastic baggie of Alvin’s droppings in the trash, likening it to his rival’s increasingly caustic ads. The beagle barks in agreement, and as Mr. Warnock declares that “we” — he and Alvin — approve of the message, the dog takes a healthy lick of his goatee."
+        "data": textToCheck
     };
     var result = "not called";
     //var data = {"name":"John", "age": 34}
